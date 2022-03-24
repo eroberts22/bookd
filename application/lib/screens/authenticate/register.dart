9d26300a1f314431740027fcc9b4dc848379693a
xavier betancourt <cbetancourt22@my.whitworth.edu>
2022-03-24 11:42:18 -0700

    Made stylistic changes to SignIn/SignUp pages, toggle between pages added

--- application/lib/screens/authenticate/register.dart
+++ application/lib/screens/authenticate/register.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:application/services/auth.dart';
 
 class Register extends StatefulWidget {
-  const Register({Key? key}) : super(key: key);
+  final Function toggleView;
+  const Register({Key? key, required this.toggleView}) : super(key: key);
 
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-  final AuthService _authService = AuthService();   // Our custom authentication class to store auth data. instance created from auth.dart class
-  final _formKey = GlobalKey<FormState>(); //key to track state of form to validate
+  final AuthService _authService =
+      AuthService(); // Our custom authentication class to store auth data. instance created from auth.dart class
+  final _formKey =
+      GlobalKey<FormState>(); //key to track state of form to validate
 
   //Store email and password local state
   String email = '';
@@ -20,54 +23,81 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[50],
+        backgroundColor: Colors.white,
         appBar: AppBar(
-            backgroundColor: Colors.cyan,
-            elevation: 0.0,
-            title: Text('Sign up for Bookd')),
+          backgroundColor: Colors.blue,
+          elevation: 0.0,
+          title: Text('BOOKD'),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () {
+                  widget.toggleView();
+                },
+                child: const Text('Sign In'),
+                style: TextButton.styleFrom(primary: Colors.white,),
+                )
+          ],
+        ),
         body: Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
             child: Form(
-              key: _formKey, //key to track state of form to validate
-                child: Column(
-                  children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val!.isEmpty ? 'Enter an email' : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                // val represents whatever was inserted
-                onChanged: (val) { // on user typing update email value
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  obscureText: true,
-                  validator: (val) => val!.length < 6 ? 'Enter a password 6 characters or longer' : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                  onChanged: (val) { // on user typing update password value
-                    setState(() => password = val);
-                  }),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                child: Text('Register', style: TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()){ // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
-                    dynamic result =  await _authService.registerWithEmailAndPassword(email, password); //get "dynamic"(result can change its type) result 
-                    if (result == null){
-                      setState(() => error = 'please supply a valid email');
-                    }
-                    // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
-                  }
-                },
-              ),
-              SizedBox(height: 12.0), //text box for error
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ]))));
+                key: _formKey, //key to track state of form to validate
+                child: Column(children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                    validator: (val) => val!.isEmpty
+                        ? 'Enter an email'
+                        : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                    // val represents whatever was inserted
+                    onChanged: (val) {
+                      // on user typing update email value
+                      setState(() => email = val);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? 'Enter a password 6 characters or longer'
+                          : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                      onChanged: (val) {
+                        // on user typing update password value
+                        setState(() => password = val);
+                      }),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                      fixedSize: MaterialStateProperty.all(const Size(300,30)),
+                    ),
+                    child:
+                        Text('Register', style: TextStyle(color: Colors.white)),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
+                        dynamic result =
+                            await _authService.registerWithEmailAndPassword(
+                                email,
+                                password); //get "dynamic"(result can change its type) result
+                        if (result == null) {
+                          setState(() => error = 'please supply a valid email');
+                        }
+                        // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
+                      }
+                    },
+                  ),
+                  SizedBox(height: 12.0), //text box for error
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  )
+                ]))));
   }
 }
--- application/lib/screens/authenticate/sign_in.dart
+++ application/lib/screens/authenticate/sign_in.dart
@@ -2,7 +2,8 @@ import 'package:application/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({Key? key}) : super(key: key);
+  final Function toggleView;
+  const SignIn({Key? key, required this.toggleView}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -20,54 +21,79 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[50],
+        backgroundColor: Colors.white,
         appBar: AppBar(
-            backgroundColor: Colors.cyan,
-            elevation: 0.0,
-            title: Text('Sign In to Bookd')),
+          backgroundColor: Colors.blue,
+          elevation: 0.0,
+          title: Text('BOOKD'),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () {
+                  widget.toggleView();
+                },
+                child: const Text('Register'),
+                style: TextButton.styleFrom(primary: Colors.white,),
+                )
+          ],
+        ),
         body: Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
             child: Form(
-              key: _formKey, //key to track state of form to validate
+                key: _formKey, //key to track state of form to validate
                 child: Column(children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val!.isEmpty ? 'Enter an email' : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                // val represents whatever was inserted
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  obscureText: true,
-                  validator: (val) => val!.length < 6 ? 'Enter a password 6 characters or longer' : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                child: Text('Sign In', style: TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  // Go log this person into firebase
-                  if (_formKey.currentState!.validate()){ // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
-                    dynamic result =  await _authService.signInWithEmailAndPassword(email, password); //get "dynamic"(result can change its type) result 
-                    if (result == null){
-                      setState(() => error = 'Invalid email or password');
-                    }
-                    // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
-                  }
-                },
-              ),
-              SizedBox(height: 12.0), //text box for error
-              Text(
-                error, // output the error from signin
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ]))));
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                    validator: (val) => val!.isEmpty
+                        ? 'Enter an email'
+                        : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                    // val represents whatever was inserted
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? 'Enter a password 6 characters or longer'
+                          : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      }),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                      fixedSize: MaterialStateProperty.all(const Size(300, 30)),
+                    ),
+                    child:
+                        Text('Sign In', style: TextStyle(color: Colors.white)),
+                    onPressed: () async {
+                      // Go log this person into firebase
+                      if (_formKey.currentState!.validate()) {
+                        // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
+                        dynamic result =
+                            await _authService.signInWithEmailAndPassword(email,
+                                password); //get "dynamic"(result can change its type) result
+                        if (result == null) {
+                          setState(() => error = 'Invalid email or password');
+                        }
+                        // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
+                      }
+                    },
+                  ),
+                  SizedBox(height: 12.0), //text box for error
+                  Text(
+                    error, // output the error from signin
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  )
+                ]))));
   }
 }
