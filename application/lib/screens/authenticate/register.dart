8b1debcae0832feadc53e29db0824be82fe3d8a2
Niklas Roberts <niklasroberts@gmail.com>
2022-04-05 10:33:17 -0700

    Fixed register bug, and unable to register with invalid profile type

--- application/lib/screens/authenticate/register.dart
+++ application/lib/screens/authenticate/register.dart
@@ -44,115 +44,115 @@ class _RegisterState extends State<Register> {
               },
             )),
         body: Container(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-                key: _formKey, //key to track state of form to validate
-                child: Column(children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                    ),
-                    validator: (val) => val!.isEmpty
-                        ? 'Enter an email'
-                        : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                    // val represents whatever was inserted
-                    onChanged: (val) {
-                      // on user typing update email value
-                      setState(() => email = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
+            child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                    key: _formKey, //key to track state of form to validate
+                    child: Column(children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        validator: (val) => val!.isEmpty
+                            ? 'Enter an email'
+                            : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                        // val represents whatever was inserted
+                        onChanged: (val) {
+                          // on user typing update email value
+                          setState(() => email = val);
+                        },
                       ),
-                      obscureText: true,
-                      validator: (val) => val!.length < 6
-                          ? 'Enter a password 6 characters or longer'
-                          : null, //indicates if form is valid or not. Using !. so assuming value won't be null
-                      onChanged: (val) {
-                        // on user typing update password value
-                        setState(() => password = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: '0 For Artist, 1 For Venue',
-                      ),
-                      validator: (val) {
-                        if (val == "0" || val == "1") {
-                          return null;
-                        } else {
-                          return "Invalid Entry. Enter 0 for Artist, 1 for Venue";
-                        }
-                      }, //indicates if form is valid or not. Using !. so assuming value won't be null
-                      onChanged: (val) {
-                        // on user typing update password value
-                        setState(() => profileType = val);
-                      }),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
-                        fixedSize:
-                            MaterialStateProperty.all(const Size(300, 30)),
-                      ),
-                      child: Text('Register',
-                          style: TextStyle(color: Colors.white)),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
-                          dynamic result =
-                              await _authService.registerWithEmailAndPassword(
-                                  email,
-                                  password); //get "dynamic"(result can change its type) result
-                          if (result == null) {
-                            setState(
-                                () => error = 'please supply a valid email');
-                          } else {
-                            if (profileType == "0" || profileType == "1") {
-                              String profile = "";
-                              if (profileType == "0") {
-                                profile = "artist";
-                              } else {
-                                profile = "venue";
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                          ),
+                          obscureText: true,
+                          validator: (val) => val!.length < 6
+                              ? 'Enter a password 6 characters or longer'
+                              : null, //indicates if form is valid or not. Using !. so assuming value won't be null
+                          onChanged: (val) {
+                            // on user typing update password value
+                            setState(() => password = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: '0 For Artist, 1 For Venue',
+                          ),
+                          validator: (val) {
+                            if (val == "0" || val == "1") {
+                              return null;
+                            } else {
+                              return "Invalid Entry. Enter 0 for Artist, 1 for Venue";
+                            }
+                          }, //indicates if form is valid or not. Using !. so assuming value won't be null
+                          onChanged: (val) {
+                            // on user typing update password value
+                            setState(() => profileType = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            fixedSize:
+                                MaterialStateProperty.all(const Size(300, 30)),
+                          ),
+                          child: Text('Register',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              // true if form is valid, false if otherwise. !NOTE!!!! Using a !. null safety operator, telling it this state can never be null. Might need to fix this to avoid bugs?
+                              // Need to check if the profile type is valid
+                              if (profileType == "0" || profileType == "1") {
+                                String profile = "";
+                                if (profileType == "0") {
+                                  profile = "artist";
+                                } else {
+                                  profile = "venue";
+                                }
+                                dynamic result = await _authService
+                                    .registerWithEmailAndPassword(email,
+                                        password); //get "dynamic"(result can change its type) result
+                                if (result == null) {
+                                  setState(() =>
+                                      error = 'please supply a valid email');
+                                } else {
+                                  String? uid = result.uid;
+                                  // Store the new user in the database!
+                                  String u_email = result.email;
+                                  Map<String, String?> user_map = {
+                                    "email": u_email,
+                                    "profileType": profile
+                                  };
+
+                                  // A new user has just been created!
+                                  DatabaseReference artistRef =
+                                      database.ref("Users/$uid");
+
+                                  await artistRef.set(user_map);
+                                  print(
+                                      "Created user $uid in database with email $u_email and profile $profile");
+                                  // if register is valid, show home page
+                                  Navigator.of(context).pushReplacementNamed('/home');
+
+                                  // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
+                                }
                               }
-                              String? uid = result.uid;
-                              // Store the new user in the database!
-                              String u_email = result.email;
-                              Map<String, String?> user_map = {
-                                "email": u_email,
-                                "profileType": profile
-                              };
-                              
-                              // A new user has just been created!
-                              DatabaseReference artistRef =
-                                  database.ref("Users/$uid");
-
-                              
-                              await artistRef.set(user_map);
-                              print(
-                                  "Created user $uid in database with email $u_email and profile $profile");
-
-                              // if register is valid, show home page
-                              if(result != null) { // if sign in is valid, show home screen
-                                Navigator.of(context).pushReplacementNamed('/home');
+                              else{
+                                setState(() =>
+                                      error = 'please supply a valid profile type');
                               }
                             }
-                          }
-                        }
-
-                        // If result is not null, the listener stream<bookduser> in auth.dart will know a user has signed in and will update authentication state
-                        
-                      }),
-                  SizedBox(height: 12.0), //text box for error
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  )
-                ])))));
+                            
+                          }),
+                      SizedBox(height: 12.0), //text box for error
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ])))));
   }
 }
