48e2c21e43a767abf8c3d7ed14fed4b5ecb30b79
Niklas Roberts <niklasroberts@gmail.com>
2022-04-07 00:29:47 -0700

    Tag functionality for venues

--- application/lib/screens/store_info/venue_store_info.dart
+++ application/lib/screens/store_info/venue_store_info.dart
@@ -2,6 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:application/services/auth.dart';
 
+import 'dart:convert';
+
 // A page for modifying settings for a signed in user who is an artist
 class venueSettings extends StatefulWidget {
   const venueSettings({Key? key}) : super(key: key);
@@ -26,6 +28,9 @@ class _venueSettingsState extends State<venueSettings> {
   String phoneNumber = "";
   // Different urls recieved through comma separated string
   String websiteLinks = "";
+
+  String error = "";
+
   Map<String, bool> tags = {
     "Bar": false,
     "Amphitheater": false,
@@ -36,7 +41,33 @@ class _venueSettingsState extends State<venueSettings> {
     "Other": false
   };
 
-  String error = "";
+  @override
+  void initState() {
+    super.initState();
+    _setupTags();
+  }
+
+  void _setupTags() async {
+    String? uid = _authService.userID;
+
+    DatabaseEvent tagEvent = await database.ref("Venues/$uid/tags").once();
+
+    Map<String, dynamic>? existingTags =
+        jsonDecode(jsonEncode(tagEvent.snapshot.value));
+
+    print(existingTags);
+    Map<String, bool> tempTags = {};
+
+    // Tags are equal to the already existing values of tags for venue, otherwise default to all false
+    if (existingTags != null) {
+      // Go through all keys and values and set all null bools to false
+      existingTags.forEach((key, value) {
+        tempTags[key] = value;
+      });
+      // Ensures that tags are set from the start
+      setState(() => tags = tempTags);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +85,8 @@ class _venueSettingsState extends State<venueSettings> {
             )),
         body: Container(
             child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 50.0),
                 child: Form(
                     key: _formKey, //key to track state of form to validate
                     child: Column(children: <Widget>[
@@ -145,6 +177,111 @@ class _venueSettingsState extends State<venueSettings> {
                             setState(() => websiteLinks = val);
                             print(websiteLinks);
                           }),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Bar"),
+                        secondary: Icon(Icons.local_bar),
+                        value: tags["Bar"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Bar"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Amphitheater"),
+                        secondary: Icon(Icons.stadium),
+                        value: tags["Amphitheater"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Amphitheater"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Lounge"),
+                        secondary: Icon(Icons.weekend),
+                        value: tags["Lounge"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Lounge"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Club"),
+                        secondary: Icon(Icons.nightlife),
+                        value: tags["Club"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Club"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Restaurant"),
+                        secondary: Icon(Icons.restaurant),
+                        value: tags["Restaurant"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Restaurant"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Outdoor"),
+                        secondary: Icon(Icons.deck),
+                        value: tags["Outdoor"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Outdoor"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
+                      const SizedBox(height: 12.0),
+                      CheckboxListTile(
+                        title: Text("Other"),
+                        secondary: Icon(Icons.info),
+                        value: tags["Other"],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            // Set the tag to the value of the checkbox, and false if null
+                            tags["Other"] = value ?? false;
+                            print(tags);
+                          });
+                        },
+                        activeColor: Colors.green,
+                        checkColor: Colors.black,
+                      ),
                       const SizedBox(height: 20.0),
                       ElevatedButton(
                         style: ButtonStyle(
@@ -171,6 +308,10 @@ class _venueSettingsState extends State<venueSettings> {
                           if (alreadyExists) {
                             // Update the existing fields
                             Map<String, Object> updateMap = {};
+
+                            // Store current tags state
+                            updateMap["tags"] = tags;
+
                             if (!name.isEmpty) {
                               updateMap["name"] = name;
                             }
@@ -204,6 +345,10 @@ class _venueSettingsState extends State<venueSettings> {
                             // Create the database reference
                             // ALL FIELDS MUST BE NON-EMPTY
                             Map<String, Object> updateMap = {};
+
+                            // Set tags
+                            updateMap["tags"] = tags;
+
                             if (!name.isEmpty) {
                               updateMap["name"] = name;
                             } else {
@@ -263,7 +408,8 @@ class _venueSettingsState extends State<venueSettings> {
                       const SizedBox(height: 12.0), //text box for error
                       Text(
                         error, // output the error from signin
-                        style: const TextStyle(color: Colors.red, fontSize: 14.0),
+                        style:
+                            const TextStyle(color: Colors.red, fontSize: 14.0),
                       )
                     ])))));
   }
